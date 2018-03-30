3634ea1d8165e2dcc8daed9763b93f3c2e4d84e0
Stanislav Kharitonov <q3introx@mail.ru>
2018-03-30 17:47:05 +0400

    Step 7

---
+++ src/games/balance.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+import createGame from '../';
+
+const defaultLowerNumber = 10;
+const defaultUpperNumber = 10000;
+
+const getRandomNumber = () => _.random(defaultLowerNumber, defaultUpperNumber, false);
+
+const getBalanceNumber = (num) => {
+  const numArr = Array.from(num.toString()).map(item => Number(item));
+  const sum = numArr.reduce((acc, e) => acc + e, 0);
+  const whole = Math.trunc(sum / numArr.length);
+  const rest = sum % numArr.length;
+
+  const result = [];
+  let count = rest;
+  for (let i = 0; i < numArr.length; i += 1) {
+    if (count) {
+      result.push(whole + 1);
+      count -= 1;
+    } else {
+      result.push(whole);
+    }
+  }
+  return result.reverse().join('');
+};
+
+export default () => {
+  const game = {
+    title: 'What is the result of the expression?',
+    getQuestion: () => {
+      const number = getRandomNumber();
+      const object = {
+        text: `${number}`,
+        correct: getBalanceNumber(number),
+      };
+
+      return object;
+    },
+  };
+  createGame(game);
+};
